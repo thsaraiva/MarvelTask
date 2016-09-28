ca96d844d67e9326c34d4000c4c681d5bcaa7add
Thiago Saraiva <thsaraiva@gmail.com>
2016-09-28 02:39:37 +0100

    creates authors data models

---
+++ app/src/main/java/com/example/thiago/saraiva/marvelcomics/Model/Marvel/Creators/MarvelCreatorsList.java
@@ -0,0 +1,35 @@
+package com.example.thiago.saraiva.marvelcomics.Model.Marvel.Creators;
+
+/**
+ * Created by thsaraiva on 23/09/2016.
+ */
+public class MarvelCreatorsList {
+    int available; //Available creators in this list. Always greater than or equal than "returned" value.,
+    int returned; //The number of creators returned in this collection (up to 20).,
+//    collectionURI (string, optional): The path to the full list of creators in this collection.,
+    MarvelCreatorSummary[]items; //The list of returned creators in this collection.
+
+    public int getAvailable() {
+        return available;
+    }
+
+    public void setAvailable(int available) {
+        this.available = available;
+    }
+
+    public int getReturned() {
+        return returned;
+    }
+
+    public void setReturned(int returned) {
+        this.returned = returned;
+    }
+
+    MarvelCreatorSummary[] getItems() {
+        return items;
+    }
+
+    public void setItems(MarvelCreatorSummary[] items) {
+        this.items = items;
+    }
+}
